23f64c100053df04983ed3125122b094bd0da851
Your Name <you@example.com>
2020-07-24 01:45:02 +0530

    Another commit during development

--- app/src/main/java/com/cbitss/sanjeewani/register_serviceprovider/register_serviceprovider.java
+++ app/src/main/java/com/cbitss/sanjeewani/register_serviceprovider/register_serviceprovider.java
@@ -1,17 +1,23 @@
 package com.cbitss.sanjeewani.register_serviceprovider;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.cbitss.sanjeewani.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class register_serviceprovider extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class register_serviceprovider extends AppCompatActivity {//implements DatePickerDialog.OnDateSetListener {
     EditText name,email,phone,dob,password;
     Button register;
     FirebaseAuth fauth;
@@ -37,7 +43,25 @@ public class register_serviceprovider extends AppCompatActivity {
                 String Phone = phone.getText().toString().trim();
             }
         });
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new dob_picker();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
 
 
     }
+
+//    @Override
+//    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//        Calendar c = Calendar.getInstance();
+//        c.set(Calendar.YEAR, year);
+//        c.set(Calendar.MONTH, month);
+//        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+//        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+//        TextView textView = (TextView) findViewById(R.id.textView);
+//        dob.setText(currentDateString);
+//    }
 }
