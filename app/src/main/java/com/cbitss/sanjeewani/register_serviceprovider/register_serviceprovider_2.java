07561ab9d844427362718d1499ef5e247e100247
Your Name <you@example.com>
2020-07-25 15:03:12 +0530

    Another commit during development

--- app/src/main/java/com/cbitss/sanjeewani/register_serviceprovider/register_serviceprovider_2.java
+++ app/src/main/java/com/cbitss/sanjeewani/register_serviceprovider/register_serviceprovider_2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +19,7 @@ public class register_serviceprovider_2 extends AppCompatActivity {
     ImageView image;
     EditText type,description;
     Button next;
+    int flag = 0;
     FirebaseFirestore fstore = FirebaseFirestore.getInstance();
 
     @Override
@@ -35,13 +37,27 @@ public class register_serviceprovider_2 extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                flag =0;
                 String Type = type.getText().toString();
                 String Description = description.getText().toString();
 
+                if(TextUtils.isEmpty(Type)){
+                    type.setError("Type is required");
+                    flag=1;
+                }
+                if(TextUtils.isEmpty(Description)){
+                    description.setError("Description is required");
+                    flag = 1;
+                }
+                else if(flag==0) {
+
+
 //                fstore.collection("")
-                //TODO: data to added to database
-                startActivity(new Intent(register_serviceprovider_2.this,register_serviceprovider_3.class));
-                finish();
+                    //TODO: data to be added to database
+                    startActivity(new Intent(register_serviceprovider_2.this, register_serviceprovider_3.class));
+                    finish();
+                }
             }
         });
     }
--- app/src/main/java/com/cbitss/sanjeewani/register_serviceprovider/register_serviceprovider_3.java
+++ app/src/main/java/com/cbitss/sanjeewani/register_serviceprovider/register_serviceprovider_3.java
@@ -3,14 +3,38 @@ package com.cbitss.sanjeewani.register_serviceprovider;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.cbitss.sanjeewani.R;
 
 public class register_serviceprovider_3 extends AppCompatActivity {
+    EditText block,street,pincode,city,state,country;
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_serviceprovider_3);
+        block = findViewById(R.id.ad1);
+        street = findViewById(R.id.ad2);
+        pincode = findViewById(R.id.pincode);
+        city = findViewById(R.id.city);
+        state = findViewById(R.id.state);
+        country = findViewById(R.id.country);
+        next = findViewById(R.id.next);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Block = block.getText().toString();
+                String Street = street.getText().toString();
+                String Pin = pincode.getText().toString();
+                String City = city.getText().toString();
+                String State = street.getText().toString();
+                String Country = country.getText().toString();
+            }
+        });
     }
 }
